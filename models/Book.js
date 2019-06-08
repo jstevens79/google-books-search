1e37f884200111c953751fd4615f1e8fded7da7a
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-08 14:41:46 -0500

    adding mongoose models

---
+++ models/Book.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  authors: {
+    type: String
+  },
+
+  description: {
+    type: String
+  },
+
+  image: {
+    type: String
+  },
+
+  link: {
+    type: String
+  }
+
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/googleBooks", { useNewUrlParser: true });
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
