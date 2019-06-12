7e58ccfdff2219ba4a0cbca7f0fe9d2058b002d8
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-12 16:14:18 -0500

    dotenv

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-let db = "mongodb://localhost/googleBooks"
-
+let db = "mongodb://localhost/googleBooks";
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  db = process.env.MONGODB_URI || "mongodb://user1:password1@ds237267.mlab.com:37267"
+  db = process.env.MONGODB_URI || process.env.PROD_DB;
 }
+
 
 mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false });
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const routes = require('./routes');
+const routes = require("./routes");
 app.use(routes);
 
 app.get("*", function(req, res) {
